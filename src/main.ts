d81b67a29937f9dcd9ecc76c8d9082efdf396885
krentor471 <lib.ruina@gmail.com>
2025-12-03 13:03:15 +0300

    Колобанов Алексеей

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 // Точка входа в ваше решение
+
+import type{
+    StringFieldValidator,
+    NumberFieldValidator,
+    ArrayFieldValidator,
+} from './types/types';
+
+function getFormElements(form: HTMLFormElement, name: string): (HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement)[] {
+  return Array.from(form.querySelectorAll(`[name="${name}"]`));
+}
+
+
+
+// import * as s from types/types.ts
+
+// validator.field(stroka).string().min('Мало символов!')
